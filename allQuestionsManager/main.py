1f42bb64062c722b4467c89692b78a57343a902b
Nickeron-dev <koshkin061969@gmail.com>
2022-01-26 17:19:46 +0200

    Finished implementing program that will update messages and send to telegram

--- allQuestionsManager/main.py
+++ allQuestionsManager/main.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 
-import io
 import os.path
 import requests
 
@@ -9,8 +8,6 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-# If modifying these scopes, delete the file token.json.
-from googleapiclient.http import MediaIoBaseDownload
 
 with open('../token_main.txt') as token_file:
     TOKEN = token_file.read()
@@ -23,17 +20,10 @@ content_previous = ''
 
 
 def main():
-    """Shows basic usage of the Drive v3 API.
-    Prints the names and ids of the first 10 files the user has access to.
-    """
     global content_previous
     creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -41,60 +31,67 @@ def main():
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
     try:
         service = build('drive', 'v2', credentials=creds)
-
+        repeated_questions = {}
         while True:
-                # with open('../previous_questions.txt', 'r') as previous_questions_file:
-                #     previous_file_id = previous_questions_file.read()
-                #
-                # previous_questions_file.close()
-                #
-                # with open('questions_illia.txt', 'r') as previous:
-                #     content_previous = previous.read()
-                #
-                # previous.close()
-                file_id = '1sqjrxQE7PruFH0qp7j8tzWUH6arJylkg'
-                # file = service.files().get(fileId=file_id).execute()
-                text = service.files().get_media(fileId=file_id).execute().decode('utf-8')
-                content_new = text
-                # request = service.files().get_media(fileId=previous_file_id)
-                # fh = io.BytesIO()
-                # downloader = MediaIoBaseDownload(fh, request)
-                # done = False
-                # while done is False:
-                #     status, done = downloader.next_chunk()
-                #     print("Download %d%%." % int(status.progress() * 100))
+            file_id_illia = '1sqjrxQE7PruFH0qp7j8tzWUH6arJylkg'
+            text_illia = service.files().get_media(fileId=file_id_illia).execute().decode('utf-8')
+            file_id_daria = '1if3GwNgPMyod72awNrHyHrMi3mZxfij2'
+            text_daria = service.files().get_media(fileId=file_id_daria).execute().decode('utf-8')
+            file_id_artem = '1lRcbZqKv1vWL-xK7vxayEha7pdjl6A2t'
+            text_artem = service.files().get_media(fileId=file_id_artem).execute().decode('utf-8')
+            content_new = 'ILLIA:\n\n' + text_illia + '\n\nDARIIA:\n\n' + text_daria + '\n\nARTEM:\n\n' + text_artem
 
-            # with io.open("new_questions_illia.txt", "wb") as illias_questions:
-            #     fh.seek(0)
-            #     illias_questions.write(fh.read())
-            #
-            # with open('new_questions_illia.txt', 'r') as new_file:
-            #     content_new = new_file.read()
-            #
-            # new_file.close()
-            #
+            if content_new != content_previous:
+                print(content_new)
+                send_text_1 = 'https://api.telegram.org/bot' + TOKEN + '/sendMessage?chat_id=' + bot_chatID.__str__() \
+                              + '&parse_mode=Markdown&text=' + 'Updated: '
+                send_text_2 = 'https://api.telegram.org/bot' + TOKEN + '/sendMessage?chat_id=' + bot_chatID.__str__() \
+                              + '&parse_mode=Markdown&text=' + content_new
+                requests.get(send_text_1)
+                requests.get(send_text_2)
+                content_previous = content_new
 
-                if content_new != content_previous:
+            line_i = ''
+            line_j = ''
+            for i in range(len(content_new)):
+                print(line_i)
+                if content_new[i] != '\n':
+                    line_i += content_new[i]
+                    continue
+                else:
+                    count = 0
+                    for j in range(i + 1, len(content_new)):
+                        if content_new[j] != '\n':
+                            line_j += content_new[j]
 
-                    # with open('questions_illia.txt', 'w') as current:
-                    #     current.write(content_new)
-                    #
-                    # current.close()
-                    print(content_new)
-                    send_text_1 = 'https://api.telegram.org/bot' + TOKEN + '/sendMessage?chat_id=' + bot_chatID.__str__() + '&parse_mode=Markdown&text=' + 'Updated:'
-                    send_text_2 = 'https://api.telegram.org/bot' + TOKEN + '/sendMessage?chat_id=' + bot_chatID.__str__() + '&parse_mode=Markdown&text=' + content_new
-                    requests.get(send_text_1)
-                    requests.get(send_text_2)
-                    content_previous = content_new
+                        else:
+                            if line_i == line_j and line_i != '' and (not repeated_questions.keys().__contains__(line_i) or repeated_questions.get(line_i) < 2):
+                                count += 1
+                                if not repeated_questions.keys().__contains__(line_i):
+                                    print('here')
+                                    repeated_questions[line_i] = 1
+                                    repeat_warning = 'WARNING!!!\nQuestion: ' + line_i + '\nHas been repeated!!!'
+                                    send_warning = 'https://api.telegram.org/bot' + TOKEN + '/sendMessage?chat_id=' \
+                                                   + bot_chatID.__str__() + '&parse_mode=Markdown&text=' + repeat_warning
+                                    requests.get(send_warning)
+                                else:
+                                    if count > 1:
+                                        print('send')
+                                        repeated_questions[line_i] += 1
+                                        repeat_warning = 'WARNING!!!\nQuestion: ' + line_i + '\nHas been repeated!!!'
+                                        send_warning = 'https://api.telegram.org/bot' + TOKEN + '/sendMessage?chat_id=' \
+                                                       + bot_chatID.__str__() + '&parse_mode=Markdown&text=' + repeat_warning
+                                        requests.get(send_warning)
 
+                            line_j = ''
+
+                    line_i = ''
     except HttpError as error:
-        # TODO(developer) - Handle errors from drive API.
         print(f'An error occurred: {error}')
 
 
